e0343af36a4736f45b45c3d6f4b52acb29442db2
ThaiciaRemigio <thais3011@gmail.com>
2026-05-12 18:10:45 -0500

    Quinto ejercicio JavaScript

---
+++ Tema05/tema05.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 3;
+
+console.log("Número A:", a);
+console.log("Número B:", b);
+
+console.log("Suma:", a + b);
+console.log("Resta:", a - b);
+console.log("Multiplicación:", a * b);
+console.log("División:", a / b);
+console.log("Residuo:", a % b);
