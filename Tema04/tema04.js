f40e9225c9159082d7109715dd91de4c27ec86ac
ThaiciaRemigio <thais3011@gmail.com>
2026-05-12 18:06:43 -0500

    Cuarto ejercicio JavaScript

---
+++ Tema04/tema04.js
@@ -0,0 +1,12 @@
+// Variable global
+let curso = "JavaScript";
+
+function mostrarInformacion() {
+    // Variable local
+    let alumno = "Thaicia";
+
+    console.log("Curso:", curso);
+    console.log("Alumno:", alumno);
+}
+
+mostrarInformacion();
