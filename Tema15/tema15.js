83f206d68632282560dd3f24a169ce1e67d96ef7
ThaiciaRemigio <thais3011@gmail.com>
2026-05-12 19:27:49 -0500

    Ejercicio 15 JavaScript

---
+++ Tema15/tema15.js
@@ -0,0 +1,14 @@
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log("Matriz completa:");
+console.log(matriz);
+
+console.log("Elemento en fila 1, columna 2:");
+console.log(matriz[0][1]);
+
+console.log("Elemento en fila 3, columna 1:");
+console.log(matriz[2][0]);
