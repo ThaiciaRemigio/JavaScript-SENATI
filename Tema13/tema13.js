80ca56fc6c74d5875a91fe95a76bf5ba94ac1102
ThaiciaRemigio <thais3011@gmail.com>
2026-05-12 19:11:44 -0500

    Ejercicio 13 JavaScript

---
+++ Tema13/tema13.js
@@ -0,0 +1,10 @@
+let frutas = ["Manzana", "Pera", "Uva", "Plátano"];
+
+console.log("Arreglo completo:");
+console.log(frutas);
+
+console.log("Primer elemento:");
+console.log(frutas[0]);
+
+console.log("Segundo elemento:");
+console.log(frutas[1]);
